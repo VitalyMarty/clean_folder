ac4aa4d99146d165c20d78444c075a7ff5cca250
Marty <vitalymarty@gmail.com>
2023-10-04 09:20:58 +0300

    Fixed setup arg

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name="clean_folder",
     version="0.0.1",
     py_modules=["clean_folder.clean"],
-    packages=['clean_folder'],
-    author='STime',
-    description='Sort and clean folders',
+    packages=["clean_folder"],
+    author="STime",
+    description="Sort and clean folders",
     entry_points={
-        "console_scripts": ["clean-folder = clean_folder.clean:main"]
-    },
-)
+        "console_scripts": [
+            "clean-folder = clean_folder.clean:main"
+            ]
+        }
+    )
